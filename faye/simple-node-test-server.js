f391d575f641b5bd43ffb7b77ed752e2281ec70e
Ian Holmes <github@yam.biowiki.org>
2013-11-24 13:14:12 -0800

    faye tests

--- faye/simple-node-test-server.js
+++ faye/simple-node-test-server.js
@@ -5,10 +5,11 @@ var server = http.createServer(),
     bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 45});
 
 // Handle non-Bayeux requests
+var n = 0;
 var server = http.createServer(function(request, response) {
     console.log(request.url);
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello, non-Bayeux request');
+  response.end('Hello, non-Bayeux request ' + (++n));
 });
 
 bayeux.attach(server);
